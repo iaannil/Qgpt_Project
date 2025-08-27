519bf4c0ec4f875f0564e0d3f5c92dfcc9d7d1e5
Anil Maharana <maharanaanil424@gmail.com>
2025-08-27 14:57:43 +0530

    Adding the backend routes for register, login and get data from user api making and that user also authotication by middleware

---
+++ backend/controllers/userController.js
@@ -0,0 +1,70 @@
+import User from '../models/User.js'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
+//function to generate jWT
+
+const generateToken = (id) =>{
+    return jwt.sign({id},process.env.JWT_SECRETE, {
+        expiresIn: '30d'
+    })
+}
+
+//Api to register user
+
+export const registerUser = async(req, res) =>{
+    const {name, email, password} = req.body;
+
+    try{
+        const userExists = await User.findOne({email})
+
+        if(userExists){
+            return res.json({success:false, message:"User already exists"})
+        }
+
+        const user = await User.create({name, email , password})
+        
+        const token = generateToken(user._id)
+        res.json({success:true , token})
+
+    }catch(err){
+         return res.json({success:false,
+            message:err.message
+         })
+    }
+}
+
+//API to login user
+
+export const loginUser = async (req,res) =>{
+    const {email, password} = req.body;
+
+    try {
+
+        const user = await User.findOne({email})
+        if(user){
+            const isMatch = await bcrypt.compare(password, user.password)
+            if(isMatch){
+                const token = generateToken(user._id)
+                return res.json({success: true, token})
+            }
+        }
+
+        return res.json({success: false, message: 'Invalid Credentials'})
+        
+    } catch (error) {
+        return res.json({success:false,message:error.message})
+    }
+}
+
+//API to get User data
+
+export const getUser = async (req,res)=>{
+    try {
+
+        const user = req.user;
+        return res.json({success:true,user})
+        
+    } catch (error) {
+         return res.json({success:false,message:error.message})
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import connectDB from './configs/db.js'
+import userRouter from './routes/userRoutes.js'
 
 
 const app = express()
 
 await connectDB()
+
+
 
 //middlewares
 app.use(cors())
@@ -16,6 +19,7 @@ app.use(express.json())
 app.get('/', (req,res)=> {
     res.send("Server is Live");
 })
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT , ()=>{
